5711cda318cfc7869ec5995d315442f1dfe2e167
ShaCode256 <ShaCode256>
2023-01-12 01:52:22 +0200

    Made a connection to server to upload requests when needed. optionally. I make it work serverless, but for presentation will uncomment the server code.

---
+++ app/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.giveandtake;
+
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
